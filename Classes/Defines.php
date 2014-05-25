cb1b337ceca01d7428a52197f71f7af294a7f147
cPJerry <jerald@cpanel.net>
2014-05-25 15:12:41 -0500

    Update Defines.php

--- Classes/Defines.php
+++ Classes/Defines.php
@@ -2,6 +2,6 @@
 define('TZ','America/Chicago');
 date_default_timezone_set(TZ);
 define('U','jerry_img');
-define('P','jer123');
+define('P','edited');
 define('H','127.0.0.1');
 define('D','jerry_img');
