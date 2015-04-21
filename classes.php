441d10eada5682d1a75223a2999814ce32559ba4
Jerald Johnson <jerry@hosterslice.com>
2015-04-21 11:51:47 -0500

    Update classes.php

--- classes.php
+++ classes.php
@@ -5,7 +5,6 @@ require_once(PATH."Classes/Defines.php");
 foreach(glob(PATH."Classes/*.php") as $file) {
   require_once($file);
 }
-if (isset($_GET['fix'])) { new AutoFixer();die();}
 if (DEV) {
   ini_set("display_errors",error_reporting(E_ALL));
 }
